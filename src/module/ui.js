dd143f0e0eaeeb0adfdaed7b8cb24e15000b57eb
Your Name <you@example.com>
2021-10-29 17:07:19 +0900

    css 수정

--- src/module/ui.js
+++ src/module/ui.js
@@ -1,5 +1,4 @@
 // 액션타입, 액션 생성함수, 리듀서
-
 export const toggleMenuOpen = { type: "MENUOPEN" };
 export const toggleSettingMenuOpen = { type: "SETTING_MENUOPEN" };
 export const toggleModalOpen = { type: "MODALOPEN" };
@@ -7,7 +6,6 @@ export const queryUpdate = (city) => ({
   type: "QUERY_UPDATE",
   city
 });
-
 export const changeColor = (bg_color) => ({
   type: "CAHNGE_COLOR",
   bg_color
@@ -20,14 +18,14 @@ export const updateItem = (state) => ({
 const initalState = {
   menuOpen: false, //메뉴 토글
   setmenuOpen: false, //세팅메뉴 토글,
+  modalOpen: false, // 모달 토글
   bg_color: "bg-blue", //현재 bg Color,
-  city: "Seoul",
-  modalOpen: false,
+  city: "Seoul", //도시 
   itemState: {
-    unit: true,
-    atmo: true,
-    sun: true,
-    wind: true
+    unit: true, //단위
+    atmo: true, //공기질
+    sun: true, //일출 일몰
+    wind: true //바람세기
   }
 };
 
